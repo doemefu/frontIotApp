eea164a1c54b868d8060d4f35e655bb7cce31ccf
Dominic Furchert <dominic.furchert@edu.tbz.ch>
2023-11-04 17:10:20 +0100

    changed url to prod

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,8 @@ module.exports = function (app) {
     app.use(
         "/api",
         createProxyMiddleware({
-            target: "http://localhost:8080",
+            //target: "http://localhost:8080",
+            target: "https://iot-app-backend.azurewebsites.net",
             changeOrigin: true,
         })
     );
